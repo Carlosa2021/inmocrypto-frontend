c7327d1c874c162cb50d364ae7c7d28ba2178336
Carlos Bernal <info@inmotoken.es>
2025-05-02 22:21:17 +0200

    home update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,27 @@
 'use client';
 
-export default function Home() {
-  const scrollToMarketplace = () => {
-    document.getElementById('listings')?.scrollIntoView({ behavior: 'smooth' });
-  };
+import Link from 'next/link';
 
+export default function Home() {
   return (
     <>
-      <main
-        className="relative min-h-[110vh] flex items-center justify-center text-center px-4 py-16 bg-cover bg-center rounded-b-3xl overflow-hidden"
-        style={{
-          backgroundImage: "url('/images/fondo.png')",
-        }}
-      >
-        {/* Solo imagen como banner visual */}
+      {/* Margen superior para separar del navbar */}
+      <div className="h-16" />
+
+      <main className="flex flex-col items-center justify-center px-4">
+        <img
+          src="/images/fondo.png"
+          alt="Banner Web3 Real Estate"
+          className="w-full max-w-xl md:max-w-3xl rounded-3xl shadow-xl"
+        />
       </main>
 
-      <section className="w-full flex justify-center py-10 ">
-        <button
-          onClick={scrollToMarketplace}
-          className="bg-indigo-600 text-white px-8 py-4 rounded-full hover:bg-indigo-700 transition text-lg font-semibold shadow-md"
-        >
-          Ver propiedades
-        </button>
+      <section className="w-full flex justify-center mt-10 mb-14">
+        <Link href="/marketplace">
+          <button className="bg-indigo-600 text-white px-8 py-4 rounded-full hover:bg-indigo-700 transition text-lg font-semibold shadow-md">
+            Ver propiedades
+          </button>
+        </Link>
       </section>
     </>
   );
