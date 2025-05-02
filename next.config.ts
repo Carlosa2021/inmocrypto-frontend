1bf44128288b24700a0c2572a832e517cc9b86d4
Carlos Bernal <info@inmotoken.es>
2025-05-02 19:14:12 +0200

    dapp ok

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 // next.config.ts
 
 const nextConfig = {
-  output: 'export',
   env: {
     NEXT_PUBLIC_THIRDWEB_CLIENT_ID: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID,
     THIRDWEB_SECRET_KEY: process.env.THIRDWEB_SECRET_KEY,
